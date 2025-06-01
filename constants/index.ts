be4837d19b66c2a84ec75e70fafb802344494173
sana <sanaashraf6666@gmail.com>
2025-06-02 03:32:44 +0530

    first commit

---
+++ constants/index.ts
@@ -0,0 +1,53 @@
+export const GenderOptions = ["Male", "Female", "Other"];
+
+export const PatientFormDefaultValues = {
+  firstName: "",
+  lastName: "",
+  email: "",
+  phone: "",
+  birthDate: new Date(Date.now()),
+  gender: "Male" as Gender,
+  address: "",
+  occupation: "",
+  emergencyContactName: "",
+  emergencyContactNumber: "",
+  primaryPhysician: "",
+  insuranceProvider: "",
+  insurancePolicyNumber: "",
+  allergies: "",
+  currentMedication: "",
+  familyMedicalHistory: "",
+  pastMedicalHistory: "",
+  identificationType: "Birth Certificate",
+  identificationNumber: "",
+  identificationDocument: [],
+  treatmentConsent: false,
+  disclosureConsent: false,
+  privacyConsent: false,
+};
+
+export const IdentificationTypes = [
+  "Birth Certificate",
+  "Driver's License",
+  "Medical Insurance Card/Policy",
+  "Military ID Card",
+  "National Identity Card",
+  "Passport",
+  "Resident Alien Card (Green Card)",
+  "Social Security Card",
+  "State ID Card",
+  "Student ID Card",
+  "Voter ID Card",
+];
+
+export const Doctor = [
+  {
+    image: "/assets/images/dr-shalima.png",
+    name: "Shalima Asharaf",
+  },
+];
+export const StatusIcon = {
+  scheduled: "/assets/icons/check.jpeg",
+  pending: "/assets/icons/pending.png",
+  cancelled: "/assets/icons/cancelled.png",
+};
